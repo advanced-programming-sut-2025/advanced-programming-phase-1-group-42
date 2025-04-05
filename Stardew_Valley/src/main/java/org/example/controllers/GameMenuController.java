f804b305a16e040e8446272c56879c5bbee1a77b
ParsaP <parsapak84@gmail.com>
2025-04-06 01:00:34 +0330

    Add files via upload
    Parsa's GameMenuController templates added

--- Stardew_Valley/src/main/java/org/example/controllers/GameMenuController.java
+++ Stardew_Valley/src/main/java/org/example/controllers/GameMenuController.java
@@ -1,5 +1,7 @@
 package org.example.controllers;
 
+import org.example.models.Result;
+
 public class GameMenuController extends Controller {
 
     //TODO: Nader
@@ -7,11 +9,27 @@ public class GameMenuController extends Controller {
     //TODO: Nader
 
     //TODO: Parsa
+    public Result cheatThunder(Int x, Int y) {
+
+    }
+    public Result cheatWeatherSet(String weather) {
+
+    }
 
     //TODO: Parsa
+    public Result walk(Int x, Int y) {
 
+    }
+    public Result printMap(Int x, Int y, Int size) {
+
+    }
     //TODO: Parsa
+    public Result cheatEnergySet(Int value) {
 
+    }
+    public Result inventoryTrashItem(String itemName, Int number) {
+
+    }
     //TODO: Arani
 
     //TODO: Arani
@@ -21,6 +39,34 @@ public class GameMenuController extends Controller {
     //TODO: Nader
 
     //TODO: Parsa
+    public Result buildBuilding(String buildingName, Int x, Int y) {
+
+    }
+    public Result buyAnimal(String animalType, String animalName) {
+
+    }
+    public Result petAnimal(String animalName) {
+
+    }
+    public Result cheatSetAnimalFriendship(String animalName , Int amount) {
+
+    }
+    public Result shepherdAnimal(String animalName, Int x , Int y) {
+
+    }
+    public Result feedHay(String animalName) {
+
+    }
+    public Result collectProduct(String animalName) {
+
+    }
+    public Result sellAnimal(String animalName) {
+
+    }
+
+    public Result fishing(String fishingPole) {
+
+    }
 
     //TODO: Nader
 
@@ -29,6 +75,17 @@ public class GameMenuController extends Controller {
     //TODO: Arani
 
     //TODO: Parsa
+    public Result tradeMoney(String receiver, String tradeType, String Item,
+                        Int amount, Int Price) {
+
+    }
+    public Result tradeGoods(String receiver, String tradeType, String Item,
+                        Int amount, String targetItem, Int targetAmount) {
+
+    }
+    public Result tradeResponse(String response, Int tradeID) {
+
+    }
 
     //TODO: Nader
 
