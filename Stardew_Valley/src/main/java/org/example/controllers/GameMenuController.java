0998365cd8d046730d32f38c0e5b3077e1805af0
Nader <87259389+Gorbekhan01@users.noreply.github.com>
2025-04-06 16:47:36 +0330

    methods & enums update

--- Stardew_Valley/src/main/java/org/example/controllers/GameMenuController.java
+++ Stardew_Valley/src/main/java/org/example/controllers/GameMenuController.java
@@ -1,10 +1,157 @@
 package org.example.controllers;
 
+import org.example.models.Result;
+
 public class GameMenuController extends Controller {
 
-    //TODO: Nader
 
-    //TODO: Nader
+    //game setting methods
+
+    public Result newGame(String username_1, String username_2, String username_3) {
+        //TODO
+    }
+
+    public Result mapGame(String mapNumber) {
+        //TODO
+    }
+
+    public Result loadGame() {
+        //TODO
+    }
+
+    public Result exitGame() {
+        //TODO
+    }
+
+    public Result nextTurn() {
+        //TODO
+    }
+
+
+    // date & time methods
+    public Result time() {
+        //TODO
+    }
+
+    public Result date() {
+        //TODO
+    }
+
+    public Result dateTime() {
+        //TODO
+    }
+
+    public Result dayOfTheWeek() {
+        //TODO
+    }
+
+    public Result showSeason() {
+        //TODO
+    }
+
+    public Result cheatAdvanceTime(String hour) {
+        //TODO
+    }
+
+    public Result cheatAdvanceDate(String date) {
+        //TODO
+    }
+
+
+    // crafting methods
+
+    public Result showCraftingRecipes() {
+        //TODO
+    }
+
+    public Result craftingCraft(String itemName) {
+        //TODO
+    }
+
+    public Result placeItem(String itemName, String direction) {
+        //TODO
+    }
+
+    public Result cheatAddItem(String itemName, String count) {
+        //TODO
+    }
+
+
+    //cooking methods
+
+    public Result cookingRefrigerator(String status, String itemName) {
+        //TODO
+    }
+
+    public Result showCookingRecipes() {
+        //TODO
+    }
+
+    public Result cookingPrepare(String recipeName) {
+        //TODO
+    }
+
+    public Result eat(String foodName) {
+        //TODO
+    }
+
+
+    // artisan methods
+
+    public Result artisanUse(String artisanName) {
+        //TODO
+    }
+
+    public Result artisanGet(String artisanName) {
+        //TODO
+    }
+
+
+    // buy & sell methods
+
+    public Result showAllProducts() {
+        //TODO
+    }
+
+    public Result showAllAvailableProducts() {
+        //TODO
+    }
+
+    public Result purchase(String productName, String count) {
+        //TODO
+    }
+
+    public Result cheatAddDollars(String count) {
+        //TODO
+    }
+
+    public Result sell(String productName, String count) {
+        //TODO
+    }
+
+
+    // NPC methods
+
+    public Result meetNPC(String npcName) {
+        //TODO
+    }
+
+    public Result giftNPC(String npcName, String itemName) {
+        //TODO
+    }
+
+    public Result friendshipNPCList() {
+        //TODO
+    }
+
+    public Result questsList() {
+        //TODO
+    }
+
+    public Result questsFinish(String index) {
+        //TODO
+    }
+
 
     //TODO: Parsa
 
@@ -16,21 +163,11 @@ public class GameMenuController extends Controller {
 
     //TODO: Arani
 
-    //TODO: Nader
-
-    //TODO: Nader
-
     //TODO: Parsa
-
-    //TODO: Nader
-
-    //TODO: Nader
 
     //TODO: Arani
 
     //TODO: Parsa
-
-    //TODO: Nader
 
 
 }
