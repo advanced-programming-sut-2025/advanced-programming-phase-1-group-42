a4e8e157a520c1b8e0ec1240b8bc5dc4b445ad85
ParsaP <parsapak84@gmail.com>
2025-04-06 19:48:47 +0330

    Add files via upload
    Parsa's phase 1 last touches

--- Stardew_Valley/src/main/java/org/example/controllers/GameMenuController.java
+++ Stardew_Valley/src/main/java/org/example/controllers/GameMenuController.java
@@ -4,90 +4,273 @@ import org.example.models.Result;
 
 public class GameMenuController extends Controller {
 
-    //TODO: Nader
 
-    //TODO: Nader
+    //game setting methods
+
+    public Result newGame(String username_1, String username_2, String username_3) {
+        //TODO
+    }
+
+    public Result mapGame(String mapNumber) {
+        //TODO
+    }
+
+    public Result loadGame() {
+        //TODO
+    }
+
+    public Result exitGame() {
+        //TODO
+    }
+
+    public Result nextTurn() {
+        //TODO
+    }
+
+
+    // date & time methods
+    public Result time() {
+        //TODO
+    }
+
+    public Result date() {
+        //TODO
+    }
+
+    public Result dateTime() {
+        //TODO
+    }
+
+    public Result dayOfTheWeek() {
+        //TODO
+    }
+
+    public Result showSeason() {
+        //TODO
+    }
+
+    public Result cheatAdvanceTime(String hour) {
+        //TODO
+    }
+
+    public Result cheatAdvanceDate(String date) {
+        //TODO
+    }
+
+
+    // crafting methods
+
+    public Result showCraftingRecipes() {
+        //TODO
+    }
+
+    public Result craftingCraft(String itemName) {
+        //TODO
+    }
+
+    public Result placeItem(String itemName, String direction) {
+        //TODO
+    }
+
+    public Result cheatAddItem(String itemName, String count) {
+        //TODO
+    }
+
+
+    //cooking methods
+
+    public Result cookingRefrigerator(String status, String itemName) {
+        //TODO
+    }
+
+    public Result showCookingRecipes() {
+        //TODO
+    }
+
+    public Result cookingPrepare(String recipeName) {
+        //TODO
+    }
+
+    public Result eat(String foodName) {
+        //TODO
+    }
+
+
+    // artisan methods
+
+    public Result artisanUse(String artisanName) {
+        //TODO
+    }
+
+    public Result artisanGet(String artisanName) {
+        //TODO
+    }
+
+
+    // buy & sell methods
+
+    public Result showAllProducts() {
+        //TODO
+    }
+
+    public Result showAllAvailableProducts() {
+        //TODO
+    }
+
+    public Result purchase(String productName, String count) {
+        //TODO
+    }
+
+    public Result cheatAddDollars(String count) {
+        //TODO
+    }
+
+    public Result sell(String productName, String count) {
+        //TODO
+    }
+
+
+    // NPC methods
+
+    public Result meetNPC(String npcName) {
+        //TODO
+    }
+
+    public Result giftNPC(String npcName, String itemName) {
+        //TODO
+    }
+
+    public Result friendshipNPCList() {
+        //TODO
+    }
+
+    public Result questsList() {
+        //TODO
+    }
+
+    public Result questsFinish(String index) {
+        //TODO
+    }
+
 
     //TODO: Parsa
+    //Weather methods
     public Result cheatThunder(Int x, Int y) {
-
+        //TODO
     }
+
+    public Result weather() {
+        //TODO
+    }
+
+    public Result weatherForecast() {
+        //TODO
+    }
+
     public Result cheatWeatherSet(String weather) {
+        //TODO
+    }
 
+    public Result greenHouseBuild() {
+        //TODO
     }
 
     //TODO: Parsa
+    //Map methods
     public Result walk(Int x, Int y) {
-
+        //TODO
     }
+
     public Result printMap(Int x, Int y, Int size) {
-
+        //TODO
     }
-    //TODO: Parsa
-    public Result cheatEnergySet(Int value) {
 
+    public Result helpReadingMap() {
+        //TODO
+    }
+
+    //TODO: Parsa
+    //inventory & Energy methods
+    public Result energyShow() {
+        //TODO
+    }
+    public Result cheatEnergySet(Int value) {
+        //TODO
+    }
+    public Result cheatEnergyUnlimited() {
+        //TODO
     }
     public Result inventoryTrashItem(String itemName, Int number) {
-
+        //TODO
     }
-    //TODO: Arani
+    public Result inventoryShow() {
+        //TODO
+    }
 
     //TODO: Arani
 
-    //TODO: Nader
-
-    //TODO: Nader
+    //TODO: Arani
 
     //TODO: Parsa
+    //Animals & Fishing methods
     public Result buildBuilding(String buildingName, Int x, Int y) {
-
+        //TODO
     }
     public Result buyAnimal(String animalType, String animalName) {
-
+        //TODO
     }
     public Result petAnimal(String animalName) {
-
+        //TODO
+    }
+    public Result animalList() {
+        //TODO
     }
     public Result cheatSetAnimalFriendship(String animalName , Int amount) {
-
+        //TODO
     }
     public Result shepherdAnimal(String animalName, Int x , Int y) {
-
+        //TODO
     }
     public Result feedHay(String animalName) {
-
+        //TODO
+    }
+    public Result animalProductionList() {
+        //TODO
     }
     public Result collectProduct(String animalName) {
-
+        //TODO
     }
     public Result sellAnimal(String animalName) {
-
+        //TODO
     }
 
     public Result fishing(String fishingPole) {
-
+        //TODO
     }
-
-    //TODO: Nader
-
-    //TODO: Nader
 
     //TODO: Arani
 
     //TODO: Parsa
-    public Result tradeMoney(String receiver, String tradeType, String Item,
-                        Int amount, Int Price) {
-
+    //Trading methods
+    public Result startTrade() {
+        //TODO
     }
-    public Result tradeGoods(String receiver, String tradeType, String Item,
+    public Result tradeWithMoney(String receiver, String tradeType, String Item,
+                        Int amount, Int Price) {
+        //TODO
+    }
+    public Result tradeWithGoods(String receiver, String tradeType, String Item,
                         Int amount, String targetItem, Int targetAmount) {
-
+        //TODO
+    }
+    public Result tradeList() {
+        //TODO
     }
     public Result tradeResponse(String response, Int tradeID) {
-
+        //TODO
     }
-
-    //TODO: Nader
-
+    public Result tradeHistory() {
+        //TODO
+    }
 
 }
--- Stardew_Valley/src/main/java/org/example/models/enums/GameMenuCommands.java
+++ Stardew_Valley/src/main/java/org/example/models/enums/GameMenuCommands.java
@@ -1,52 +1,137 @@
 package org.example.models.enums;
 
+import org.example.models.Result;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum GameMenuCommands implements Command {
-    //TODO: Nader
 
-    //TODO: Nader
 
-    //TODO: Parsa
 
     //TODO: Parsa
+    //Weather
+    CHEAT_THUNDER(""),
+    WEATHER(""),
+    WEATHER_FORECAST(""),
+    CHEAT_WEATHER_SET(""),
+    GREEN_HOUSE_BUILD(""),
 
     //TODO: Parsa
+    //Map
+    WALK(""),
+    PRINT_MAP(""),
+    HELP_READING_MAP(""),
+
+    //TODO: Parsa
+    //inventory & Energy
+    ENERGY_SHOW(""),
+    CHEAT_ENERGY_SET(""),
+    CHEAT_ENERGY_UNLIMITED(""),
+    INVENTORY_TRASH_ITEM(""),
+    INVENTORY_SHOW(""),
 
     //TODO: Arani
 
     //TODO: Arani
 
-    //TODO: Nader
+    // Game Commands
 
-    //TODO: Nader
+    NEW_GAME(""),
+    GAME_MAP(""),
+    LOAD_GAME(""),
+    EXIT_GAME(""),
+    NEXT_TURN(""),
+
+
+    //  Date & Time Commands
+    TIME(""),
+    DATE(""),
+    DATE_TIME(""),
+    DAY_OF_THE_WEEK(""),
+    SEASON(""),
+    // --> cheat codes
+    CHEAT_ADVANCE_TIME(""),
+    CHEAT_ADVANCE_DATE(""),
+
+
+    // Crafting Commands
+    SHOW_CRAFTING_RECIPES(""),
+    CRAFTING_CRAFT(""),
+    PLACE_ITEM(""),
+    // --> cheat codes
+    CHEAT_ADD_ITEM(""),
+
+
+    // Cooking Commands
+    COOKING_REFRIGERATOR(""),
+    SHOW_COOKING_RECIPES(""),
+    COOKING_PREPARE(""),
+    EAT(""),
+
+
+    // Artisan Commands
+    ARTISAN_USE(""),
+    ARTISAN_GET(""),
+
+
+    // Sell & Buy Commands
+    SHOW_ALL_PRODUCTS(""),
+    SHOW_ALL_AVAILABLE_PRODUCTS(""),
+    PURCHASE(""),
+    SELL(""),
+    // --> cheat codes
+    CHEAT_ADD_DOLLARS(""),
+
+    // NPC
+    MEET_NPC(""),
+    GIFT_NPC(""),
+    FRIENDSHIP_NPC_LIST(""),
+    QUESTS_LIST(""),
+    QUESTS_FINISH(""),
+
+
 
     //TODO: Parsa
-
-    //TODO: Nader
-
-    //TODO: Nader
+    //Animals & Fishing
+    BUILD_BUILDING(""),
+    BUY_ANIMAL(""),
+    PET_ANIMAL(""),
+    ANIMAL_LIST(""),
+    CHEAT_SET_ANIMAL_FRIENDSHIP(""),
+    SHEPHERD_ANIMAL(""),
+    FEED_HAY(""),
+    ANIMAL_PRODUCTION_LIST(""),
+    COLLECT_PRODUCT(""),
+    SELL_ANIMAL(""),
+    FISHING(""),
 
     //TODO: Arani
 
     //TODO: Parsa
+    //Trading
+    START_TRADE(""),
+    TRADE_WITH_MONEY(""),
+    TRADE_WITH_GOODS(""),
+    TRADE_LIST(""),
+    TRADE_RESPONSE(""),
+    TRADE_HISTORY("");
 
-    //TODO: Nader
 
     private final String pattern;
+
     GameMenuCommands(String pattern) {
         this.pattern = pattern;
     }
 
 
     @Override
-    public Matcher matcher(String input){
+    public Matcher matcher(String input) {
         Matcher matcher = Pattern.compile(this.pattern).matcher(input);
 
-        if(matcher.matches())
+        if (matcher.matches())
             return matcher;
 
         return null;
     }
-}
+    }
