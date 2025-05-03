97ad2701eb9b7078fd1d539c39d66209ac009404
MohammadParsa MahmoudabadiArani <Parsa.Arani0684@gmail.com>
2025-05-03 18:33:18 +0330

    Blacksmith Developed

--- Stardew_Valley/src/main/java/org/example/models/Pair.java
+++ Stardew_Valley/src/main/java/org/example/models/Pair.java
@@ -1,5 +1,27 @@
 package org.example.models;
 
-public record Pair<A, B>(A first, B second) {
+public class Pair<A, B> {
+    private A first;
+    private B second;
 
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A first() {
+        return first;
+    }
+
+    public B second() {
+        return second;
+    }
+
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    public void setSecond(B second) {
+        this.second = second;
+    }
 }
--- Stardew_Valley/src/main/java/org/example/models/goods/tools/ToolLevel.java
+++ Stardew_Valley/src/main/java/org/example/models/goods/tools/ToolLevel.java
@@ -2,17 +2,23 @@ package org.example.models.goods.tools;
 
 import org.example.models.goods.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public enum ToolLevel {
-    ORDINARY(0),
-    COPPER(1),
-    IRON(2),
-    GOLD(3),
-    IRIDIUM(4);
+    ORDINARY(0, "Ordinary"),
+    COPPER(1, "Copper"),
+    IRON(2, "Iron"),
+    GOLD(3, "Gold"),
+    IRIDIUM(4, "Iridium");
 
     private int levelNumber;
+    private String name;
+    public final static ArrayList<ToolLevel> toolLevels = new ArrayList<>(Arrays.asList(values()));
 
-    ToolLevel(int levelNumber) {
+    ToolLevel(int levelNumber, String name) {
         this.levelNumber = levelNumber;
+        this.name = name;
     }
 
     public int getLevelNumber() {
@@ -35,5 +41,9 @@ public enum ToolLevel {
                 return IRIDIUM;
         }
         return null;
+    }
+
+    public String getName() {
+        return name;
     }
 }
