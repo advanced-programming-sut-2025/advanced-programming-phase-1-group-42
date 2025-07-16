063b451c569a5e30cb6ea7b6e80903f2bd289780
MohammadParsa MahmoudabadiArani <Parsa.Arani0684@gmail.com>
2025-07-16 17:21:06 +0330

    fix: Main View developed

--- StardewValley/core/src/main/java/com/StardewValley/controllers/MainMenuController.java
+++ StardewValley/core/src/main/java/com/StardewValley/controllers/MainMenuController.java
@@ -48,43 +48,4 @@ public class MainMenuController extends Controller {
             Main.getMain().setScreen(new LoginMenuView(new LoginRegisterMenuController(), Assets.getInstance().getSkin()));
         }
     }
-
-    public Result menuEnter(String menuName) {
-        switch (menuName) {
-            case "avatar":
-                break;
-            case "profile":
-                App.setCurrentMenu(Menu.ProfileMenu);
-                break;
-            case "game":
-                App.setCurrentMenu(Menu.GameMenu);
-                break;
-            default:
-                return new Result(false, "Invalid menu name");
-        }
-
-        return new Result(true, "Redirecting to " + menuName + "!");
-    }
-
-    public Result exit() {
-//        DBInteractor.saveUsers();
-//        DBInteractor.setStayLogin();
-        App.setCurrentMenu(Menu.ExitMenu);
-        return new Result(true, "Goodbye!");
-    }
-
-    public Result logout() {
-        if (App.getCurrentUser().isStayLogin()) {
-//            DBInteractor.resetStayLogin();
-        }
-//        DBInteractor.saveUsers();
-        App.getCurrentUser().setStayLogin(false);
-        App.setCurrentUser(null);
-
-        return new Result(true, "You have logged out! Redirecting to login/register menu!");
-    }
-
-    public Result showCurrentMenu() {
-        return new Result(true, "Current Menu : Main Menu");
-    }
 }
