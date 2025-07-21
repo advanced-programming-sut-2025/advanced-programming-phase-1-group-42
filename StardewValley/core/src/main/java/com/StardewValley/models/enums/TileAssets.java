b94ed5838adc65c3f92c8d7c2f85103aa715847a
MohammadParsa MahmoudabadiArani <Parsa.Arani0684@gmail.com>
2025-07-21 20:29:32 +0330

    feat: Tile Assets developed

---
+++ StardewValley/core/src/main/java/com/StardewValley/models/enums/TileAssets.java
@@ -0,0 +1,31 @@
+package com.StardewValley.models.enums;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public enum TileAssets {
+    GRASS(new Texture("GameAssets/Flooring/Flooring_50.png")),
+    FARM_ORDINARY(new Texture("GameAssets/Flooring/Flooring_14.png")),
+    FARM_WINDER(new Texture("GameAssets/Flooring/Flooring_38.png")),
+    FARM_PLOWED(new Texture("GameAssets/Flooring/Flooring_21.png")),
+    FARM_WET(new Texture("GameAssets/Flooring/Flooring_62.png")),
+    WATER(new Texture("GameAssets/Flooring/Flooring_26.png")),
+    BEACH(new Texture("GameAssets/Flooring/Flooring_25.png")),
+    GREEN_HOUSE(new Texture("GameAssets/Greenhouse/greenhouse.png")),
+    QUARRY(new Texture("GameAssets/Flooring/Flooring_72.png")),
+    STONE_WALL(new Texture("GameAssets/Flooring/Flooring_52 .png")),
+    SQUARE(new Texture("GameAssets/Flooring/Flooring_16.png")),
+    ROAD(new Texture("GameAssets/Flooring/Flooring_48.png")),
+    SHIPPING_BIN(new Texture("GameAssets/Shipping_Bin/Shipping_Bin.png")),
+    JOJAMART(new Texture("GameAssets/Game_Buildings/Jojamart.png")),
+    MARNIE_RANCH(new Texture("GameAssets/Game_Buildings/Ranch.png")),
+    PIERRE_SHOP(new Texture("GameAssets/Game_Buildings/Pierres_shop.png")),
+    THE_STARDROP_SALOON(new Texture("GameAssets/Game_Buildings/Saloon.png")),
+    BLACKSMITH(new Texture("GameAssets/Game_Buildings/Blacksmith.png")),
+    CARPENTER_SHOP(new Texture("GameAssets/Game_Buildings/Carpenter_Shop.png")),
+    FISH_SHOP(new Texture("GameAssets/Game_Buildings/Fish_Shop.png"));
+
+    private Texture texture;
+    TileAssets(Texture texture) {
+        this.texture = texture;
+    }
+}
